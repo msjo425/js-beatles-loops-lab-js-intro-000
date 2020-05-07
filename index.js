d579da9011f417cb5ec0ce8b3f68b96c41577951
matt joseloff <msjo425@gmail.com>
2020-05-07 20:12:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function johnLennonFacts(array) {
 function iLoveTheBeatles(counter){
   const array = []
   do {
-  array.push("I love the beatles")
+  array.push("I love the beatles!")
   counter += 1
   }
   while (counter < 15)
