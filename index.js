6fa98e8346f8cc767464522ca797d6a346c9636d
matt joseloff <msjo425@gmail.com>
2020-05-07 19:16:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+var holder = []
+let i = 0
+function theBeatlesPlay(names,instruments){
+for (i=0; i < names.length; i++){
+  holder.push(`${names[i]} plays ${instruments[i]}`)
+}
+return holder
+i = 0
+}
+
+
+function johnLennonFacts(array) {
+  var i = 0
+  while (i < array.length) {
+    array[i] += '!!!';
+    i++;
+  }
+  return array;
+}
